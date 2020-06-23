511716acda53f38db39fc3cf7e8fa396b5da9efe
Thomas <thom97m6@ITSKP-Odense.dk>
2020-06-23 14:11:34 +0200

    Rettet paa connection func.

--- Connection/Conn.php
+++ Connection/Conn.php
@@ -1,1 +1,19 @@
+<?php
 
+    function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "Portfolie";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+ 
+ 
